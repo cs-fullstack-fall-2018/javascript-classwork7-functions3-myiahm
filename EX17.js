19bab409caa0d05dd81f76874cf6948d22c228b9
Myiah Moore <myiahsade@gmail.com>
2018-07-31 14:33:32 -0500

    check in

---
+++ EX17.js
@@ -0,0 +1,14 @@
+main();
+
+function main()
+{
+var number1 = 15;
+var number2 = 45;
+var number3 = 47;
+alert(printSum(number1+number2+number3))
+}
+function printSum(num1,num2,num3)
+{
+    var result = num1+num2+num3;
+    return result
+}
---
+++ EX18.js
@@ -0,0 +1,21 @@
+main();
+
+function main()
+{
+var number1 = 23;
+var number2 = 45;
+    alert(product(number1,number2))
+    alert(quotient(number1,number2))
+}
+
+function product(num1,num2)
+{
+    var result = num1*num2
+    return result
+}
+
+function quotient(num11,num22) 
+{
+    var result = num11/num22
+    return result
+}
